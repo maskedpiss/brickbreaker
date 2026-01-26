a9cf5024cd8a9c904d1efe4b8828b9407c601963
maskedpiss <joshkeckedits@proton.me>
2026-01-26 15:29:55 -0600

    re-instanced paddle in play state

--- src/states/play.lua
+++ src/states/play.lua
@@ -1,17 +1,27 @@
 local Play = {}
 
+local paddle = nil
+
 function Play.onEnter()
   Globals.mouseVisible = false
   love.mouse.setVisible(Globals.mouseVisible)
+  
+  Globals.Paddle = require("src/objs/paddle")
+  paddle = Globals.Paddle.new()
 end
 
 
 function Play.update(dt)
-  
+  paddle:update(dt)
 end
 
 
 function Play.draw()
+  paddle:draw()
+end
+
+
+function Play.onExit()
   
 end
 
