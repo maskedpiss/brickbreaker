3fb7bdbfe1ea7306f5f62a708688cf69167eb2a1
maskedpiss <joshkeckedits@proton.me>
2026-01-26 11:42:26 -0600

    ball now bounces off the walls and ceiling

--- src/objs/ball.lua
+++ src/objs/ball.lua
@@ -19,6 +19,19 @@ end
 function Ball:update(dt)
   self.x = self.x + self.xVel * dt
   self.y = self.y + self.yVel * dt
+  
+  if self.x < Globals.Screen.x then
+    self.x = Globals.Screen.x
+    self.xVel = -self.xVel
+  elseif self.x + self.width > Globals.Screen.width then
+    self.x = Globals.Screen.width - self.width
+    self.xVel = -self.xVel
+  end
+  
+  if self.y < Globals.Screen.y then
+    self.y = Globals.Screen.y
+    self.yVel = -self.yVel
+  end
 end
 
 
