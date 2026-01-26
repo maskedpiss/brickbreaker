64c86b82899e280ec68c2f7be57042879687fdeb
maskedpiss <joshkeckedits@proton.me>
2026-01-26 13:50:49 -0600

    drew title

--- src/states/menu.lua
+++ src/states/menu.lua
@@ -3,6 +3,13 @@ local Menu = {}
 function Menu.onEnter()
   Globals.mouseVisible = true
   love.mouse.setVisible(Globals.mouseVisible)
+  
+  Menu.Title = {
+      font = love.graphics.newFont(128),
+      text = "Brick Breaker",
+      x = Globals.Screen.x,
+      y = 50
+  }
 end
 
 
@@ -12,7 +19,9 @@ end
 
 
 function Menu.draw()
-  
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.setFont(Menu.Title.font)
+  love.graphics.printf(Menu.Title.text, Menu.Title.x, Menu.Title.y, Globals.Screen.width, "center")
 end
 
 
