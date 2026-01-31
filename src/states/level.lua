e5a798d65a9363f38caefd52872aadcc3e882736
maskedpiss <joshkeckedits@proton.me>
2026-01-31 10:54:52 -0600

    level transition now displays which level it is

--- src/states/level.lua
+++ src/states/level.lua
@@ -1,7 +1,11 @@
 local Level = {}
 
 function Level.onEnter()
-  
+  Level.Text = {
+      font = love.graphics.newFont(128),
+      x = Globals.Screen.x,
+      y = (Globals.Screen.height / 2) - 128
+  }
 end
 
 
@@ -11,7 +15,9 @@ end
 
 
 function Level.draw()
-  
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.setFont(Level.Text.font)
+  love.graphics.printf("Level "..Globals.level, Level.Text.x, Level.Text.y, Globals.Screen.width, "center")
 end
 
 
