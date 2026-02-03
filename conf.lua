e28fcd7bb8c4ac2d3169e649430f73594450a25e
maskedpiss <joshkeckedits@proton.me>
2026-02-03 12:09:29 -0600

    rewrote conf

--- conf.lua
+++ conf.lua
@@ -1,9 +1,13 @@
 function love.conf(t)
-    t.title = "Brick Breaker"
+    t.identiy = "brick_breaker"
     t.version = "11.5"
     
+    t.window.title = "Brick Breaker"
     t.window.width = 1280
     t.window.height = 720
+    t.window.vsync = 1
+    t.window.resizable = false
     
-    t.vsync = true
+    t.modules.physics = false
+    t.modules.joystick = false
 end
